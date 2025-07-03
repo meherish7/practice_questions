7512416824d3cba8b4761052a0def474a1606d9f
MEGHANA <MEGHANA@DESKTOP-JUD0BAV>
2025-07-03 12:44:40 +0530

    updated palindrome

--- arrays/src/arrays/Palindrome.java
+++ arrays/src/arrays/Palindrome.java
@@ -37,36 +37,3 @@ public class Palindrome {
 		
 	}
 }
-		// TODO Auto-generated method stub
-//		int arr[]= {1,2,3,2,1};
-//		int size=arr.length;
-//		System.out.println(size);
-//		int[] temp=new int[5];
-//		int count=0;
-//		for(int i=size-1;i>=0;i--) {
-//			temp[count]=arr[i];
-//			count++;
-//				}
-//		boolean flag=true;
-//		for(int i=0;i<size;i++) {
-//		  if(arr[i]==temp[i]){
-//			  
-//		  }
-//		  else {
-//			    flag=false; 
-//			    break;
-//		  }
-//			
-//			
-//		}
-//		if(flag==true) {
-//			System.out.println("palindrome");
-//		}else {
-//			System.out.println("not a palindreome");
-//		}
-//		
-//
-//	
-//
-//}
-//}

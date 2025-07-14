124d6da3e1c2018f80ae9dedd1dc869cca8ec629
MEGHANA <MEGHANA@DESKTOP-JUD0BAV>
2025-07-14 12:08:43 +0530

    coommited

--- arrays/src/arrays/insert.java
+++ arrays/src/arrays/insert.java
@@ -2,17 +2,17 @@ package arrays;
 import java.util.ArrayList;
 public class insert {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<String> arr=new ArrayList<>();
-		arr.add("apple");
-		arr.add("banana");
-		arr.add("pineapple");
-		System.out.println("before"+arr);
-		arr.add(0,"pink");
-		System.out.println("after"+arr);
-		
-		
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		ArrayList<String> arr=new ArrayList<>();
+//		arr.add("apple");
+//		arr.add("banana");
+//		arr.add("pineapple");
+//		System.out.println("before"+arr);
+//		arr.add(0,"pink");
+//		System.out.println("after"+arr);
+//		
+//		
+//	}
 
 }
